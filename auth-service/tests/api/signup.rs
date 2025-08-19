e103f2ab5f25f8664e119b0e9c855a54947308f8
loren.severs <loren.severs@equipmentshare.com>
2025-08-18 19:13:55 -0500

    [Sprint 1][Task 7] feat: add 422 for malformed signup

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -1,10 +1,39 @@
-use crate::helpers::TestApp;
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn post_signup_route() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    let random_email = get_random_email();
 
-    assert_eq!(response.status().as_u16(), 200);
+    let test_cases = [
+        serde_json::json!({
+        "ema": random_email,
+        "password": "password123",
+        "requires2FA": true
+        }),
+        serde_json::json!({
+        "password": "password123",
+        "requires2FA": true
+        }),
+        serde_json::json!({
+        "email": random_email,
+        "password": 123,
+        "requires2FA": true
+        }),
+        serde_json::json!({
+        "ema": random_email,
+        "password": "password123",
+        "requires2FA": "abc123"
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {test_case}"
+        )
+    }
 }
