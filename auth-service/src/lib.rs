35c3187a2d996ba41cb9808cfd492c48a6609dbf
loren.severs <loren.severs@equipmentshare.com>
2025-09-02 08:53:14 -0500

    deploy sprint 2

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,9 +1,22 @@
 use std::error::Error;
 
-use axum::{routing::post, serve::Serve, Router};
+use app_state::AppState;
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::post,
+    serve::Serve,
+    Json, Router,
+};
+use domain::AuthAPIError;
+use routes::{login, logout, signup, verify_2fa, verify_token};
+use serde::{Deserialize, Serialize};
 use tower_http::services::ServeDir;
 
+pub mod app_state;
+pub mod domain;
 pub mod routes;
+pub mod services;
 
 pub struct Application {
     server: Serve<Router, Router>,
@@ -11,25 +24,48 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+    pub async fn build(app_state: AppState, address: &str) -> Result<Self, Box<dyn Error>> {
+
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
             .route("/verify-token", post(routes::verify_token))
             .route("/signup", post(routes::signup))
             .route("/login", post(routes::login))
             .route("/logout", post(routes::logout))
-            .route("/verify-2fa", post(routes::verify_2fa));
+            .route("/verify-2fa", post(routes::verify_2fa))
+            .with_state(app_state);
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
 
-        Ok(Self { server, address })
+        Ok(Application { server, address })
     }
 
     pub async fn run(self) -> Result<(), std::io::Error> {
         println!("listening on {}", &self.address);
         self.server.await?;
         Ok(())
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub error: String,
+}
+
+impl IntoResponse for AuthAPIError {
+    fn into_response(self) -> Response {
+        let (status, error_message) = match self {
+            AuthAPIError::UserAlreadyExists => (StatusCode::CONFLICT, "User already exists"),
+            AuthAPIError::InvalidCredentials => (StatusCode::BAD_REQUEST, "Invalid credentials"),
+            AuthAPIError::UnexpectedError => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected error")
+            }
+        };
+        let body = Json(ErrorResponse {
+            error: error_message.to_string(),
+        });
+        (status, body).into_response()
     }
 }
