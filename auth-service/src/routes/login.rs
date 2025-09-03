e0e07cbb2b45a6a79531a52b3b6ec263559afe89
loren.severs <loren.severs@equipmentshare.com>
2025-09-03 17:22:31 -0500

    [Sprint 3][Task 2] Added Cookie Jar into login, tests are passing

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,22 +1,40 @@
-use crate::domain::{AuthAPIError, Email, Password};
-use crate::AppState;
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, Email, Password},
+    utils::auth::generate_auth_cookie,
+};
 use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
 use serde::{Deserialize, Serialize};
 
 pub async fn login(
     State(state): State<AppState>,
+    jar: CookieJar,
     Json(request): Json<LoginRequest>,
-) -> Result<impl IntoResponse, AuthAPIError> {
-    let email =
-        Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
-    let password =
-        Password::parse(request.password.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    let email = match Email::parse(request.email.clone()) {
+        Ok(email) => email,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
+
+    let password = match Password::parse(request.password.clone()) {
+        Ok(password) => password,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
+
     let user_store = &state.user_store.read().await;
 
-    let validate = user_store.validate_user(&email, &password).await;
-    match validate {
-        Err(_) => Err(AuthAPIError::IncorrectCredentials),
-        Ok(_) => Ok(StatusCode::OK.into_response()),
+    match user_store.validate_user(&email, &password).await {
+        Err(_) => (jar, Err(AuthAPIError::IncorrectCredentials)),
+        Ok(_) => {
+            let auth_cookie = match generate_auth_cookie(&email) {
+                Ok(cookie) => cookie,
+                Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+            };
+
+            let updated_jar = jar.add(auth_cookie);
+            (updated_jar, Ok(StatusCode::OK.into_response()))
+        }
     }
 }
 
