a8a384cdd5b752c20d164bad16c7b7684a40ea1d
loren.severs <loren.severs@equipmentshare.com>
2025-09-12 21:53:23 -0500

    [Sprint 4][Task 3] 200 test working

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -1,20 +1,54 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
 use serde::Deserialize;
 
-use crate::domain::{AuthAPIError, Email, LoginAttemptId, TwoFACode};
-
+use crate::{
+    app_state::AppState,
+    domain::{AuthAPIError, Email, LoginAttemptId, TwoFACode}, utils::auth::generate_auth_cookie,
+};
 
 pub async fn verify_2fa(
+    State(state): State<AppState>,
+    jar: CookieJar,
     Json(request): Json<Verify2FARequest>,
-) -> Result<impl IntoResponse, AuthAPIError> {
-    
-    let email = Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
-    let login_attempt_id = LoginAttemptId::parse(request.login_attempt_id.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
-    let two_fa_code = TwoFACode::parse(request.two_fa_code.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
+) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
+    let two_fa_code_store = state.two_fa_code_store.write().await;
 
-    
-    Ok(StatusCode::OK.into_response())
+    let email = match Email::parse(request.email.clone()) {
+        Ok(email) => email,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
+    let login_attempt_id = match LoginAttemptId::parse(request.login_attempt_id.clone()) {
+        Ok(login_attempt_id) => login_attempt_id,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
+    let two_fa_code = match TwoFACode::parse(request.two_fa_code.clone()) {
+        Ok(two_fa_code) => two_fa_code,
+        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
+    };
 
+    let code_tuple = match two_fa_code_store
+        .get_code(&email)
+        .await {
+            Ok(code_tuple) => code_tuple,
+            Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
+        };
+
+    if code_tuple.0 != login_attempt_id {
+        return (jar, Err(AuthAPIError::IncorrectCredentials));
+    }
+    if code_tuple.1 != two_fa_code {
+        return (jar, Err(AuthAPIError::IncorrectCredentials));
+    }
+
+    let auth_cookie = match generate_auth_cookie(&email) {
+        Ok(cookie) => cookie,
+        Err(_) => return (jar, Err(AuthAPIError::UnexpectedError)),
+    };
+
+    let updated_jar = jar.add(auth_cookie);
+
+    (updated_jar, Ok(StatusCode::OK.into_response()))
 }
 
 #[derive(Deserialize)]
--- auth-service/tests/api/verify_2fa.rs
+++ auth-service/tests/api/verify_2fa.rs
@@ -1,4 +1,9 @@
-use auth_service::domain::LoginAttemptId;
+use auth_service::{
+    domain::{Email, LoginAttemptId, TwoFACode, TwoFACodeStore},
+    routes::TwoFactorAuthResponse,
+    utils::constants::JWT_COOKIE_NAME,
+    ErrorResponse,
+};
 
 use crate::helpers::{get_random_email, TestApp};
 
@@ -27,4 +32,102 @@ async fn should_return_400_if_invalid_input() {
 
     let response = app.post_verify_2fa(&invalid_request).await;
     assert_eq!(response.status().as_u16(), 400)
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+
+    let response = app.post_login(&login_body).await;
+
+    assert_eq!(response.status().as_u16(), 206);
+
+    let response_body = response
+        .json::<TwoFactorAuthResponse>()
+        .await
+        .expect("Could not deserialize response body to TwoFactorAuthResponse");
+
+    let two_fa_body = serde_json::json!({
+        "email": random_email,
+        "loginAttemptId": response_body.login_attempt_id,
+        "2FACode": "123456",
+    });
+
+    let response = app.post_verify_2fa(&two_fa_body).await;
+
+    assert_eq!(response.status().as_u16(), 401);
+
+}
+
+// #[tokio::test]
+// async fn should_return_401_if_old_code() {
+//     // Call login twice. Then, attempt to call verify-fa with the 2FA code from the first login requet. This should fail. 
+//     todo!()
+// }
+
+#[tokio::test]
+async fn should_return_200_if_correct_code() {
+
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let signup_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+        "requires2FA": true
+    });
+
+    let response = app.post_signup(&signup_body).await;
+
+    assert_eq!(response.status().as_u16(), 201);
+
+    let login_body = serde_json::json!({
+        "email": random_email,
+        "password": "password123",
+    });
+
+    let response = app.post_login(&login_body).await;
+
+    assert_eq!(response.status().as_u16(), 206);
+
+    let response_body = response
+        .json::<TwoFactorAuthResponse>()
+        .await
+        .expect("Could not deserialize response body to TwoFactorAuthResponse");
+
+    let two_fa_code = app.two_fa_code_store.read().await.get_code(&Email::parse(random_email.clone()).unwrap()).await.expect("2FA code not found");
+
+    let two_fa_body = serde_json::json!({
+        "email": random_email,
+        "loginAttemptId": response_body.login_attempt_id,
+        "2FACode": two_fa_code.1.as_ref().to_string(),
+    });
+
+    let response = app.post_verify_2fa(&two_fa_body).await;
+    assert_eq!(response.status().as_u16(), 200);
+
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("No auth cookie found");
+
+    assert!(!auth_cookie.value().is_empty());
 }
